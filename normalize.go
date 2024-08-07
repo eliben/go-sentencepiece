66bf48a8fd7dd8cd950722a07a13a6e0fa425114
Eli Bendersky <eliben@gmail.com>
2024-08-07 07:10:33 -0700

    Implement the system test

--- normalize.go
+++ normalize.go
@@ -5,7 +5,7 @@ import "strings"
 // normalize performs unicode normalization.
 //
 // SentencePiece has a feature to perform configurable unicode normalization on
-// the input text and has some options for adding dummpy whitespace prefixes or
+// the input text and has some options for adding dummy whitespace prefixes or
 // trimming whitespace. However, the model we're working with has a very simple
 // normalizer that does none of this. These options can be added in the future
 // if needed.

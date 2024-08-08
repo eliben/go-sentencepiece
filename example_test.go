31da96365ad96e206c638e1d7f1031b7f15107ca
Eli Bendersky <eliben@gmail.com>
2024-08-08 06:23:29 -0700

    Add example

---
+++ example_test.go
@@ -0,0 +1,29 @@
+package sentencepiece_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/eliben/go-sentencepiece"
+)
+
+func ExampleEncode() {
+	protoFile := os.Getenv("MODELPATH")
+	if protoFile == "" {
+		log.Println("Need MODELPATH env var to run example")
+		return
+	}
+
+	enc, err := sentencepiece.NewEncoder(protoFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := "Encoding produces tokens that LLMs can learn and understand"
+	tokens := enc.Encode(text)
+
+	for _, token := range tokens {
+		fmt.Println(token)
+	}
+}
--- token.go
+++ token.go
@@ -1,7 +1,13 @@
 package sentencepiece
 
+import "fmt"
+
 // Token represents a single token from the input text
 type Token struct {
 	ID   int
 	Text string
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("Token{ID: %v, Text: %q}", t.ID, t.Text)
 }
